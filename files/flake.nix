c002e4226b27881433a0fee2a128aee598bdd0f3
Mukul_DK <126465196+Mukulkalsait@users.noreply.github.com>
2025-08-23 14:37:30 +0530

    Update flake.nix

--- files/flake.nix
+++ files/flake.nix
@@ -1,26 +1,39 @@
 {
-  description = "Mukul's NixOS with Home Manager";
+  description = "Mukul's NixOS with Hyprland and Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # stable channel
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
-      nixosConfigurations.mukul = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.mukul-nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           ./configuration.nix
+          hyprland.nixosModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.mukul = import ./home.nix;
+            home-manager.extraSpecialArgs = { inherit hyprland; };
           }
         ];
       };
